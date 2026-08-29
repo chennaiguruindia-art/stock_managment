59dee1c5cdbe7548a69763dbcd4c17e966037a4d
Rajesh Gurusamy <chennaiguruindia@gmail.com>
2026-08-29 18:03:06 +0530

    update_web.php

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,15 @@ Route::post('/pos/checkout', [MainController::class, 'posCheckout'])->name('pos_
 Route::get('/sales-history', [indexController::class, 'sales_history'])->name('sales_history');
 Route::get('/report', [indexController::class, 'report'])->name('report');
 Route::get('/logout', [indexController::class, 'logout'])->name('logout');
+
+// TEMPORARY: visit once to create database tables, then delete this route.
+Route::get('/run-migrations-c38c8be29fcde72095fb90fb', function () {
+\Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+return '<pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
+});
+
+// TEMPORARY: visit once to create database tables, then delete this route.
+Route::get('/run-migrations-fresh', function () {
+\Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--force' => true]);
+return '<pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
+});
